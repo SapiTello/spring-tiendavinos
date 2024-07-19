e9780c87c2ea951080311c7de0822a77c8b73b83
SapiTello <saidapi@hotmail.com>
2024-07-19 01:50:57 -0500

    Agregar el pagina show

---
+++ src/main/java/com/tiendavinos/service/ProductoService.java
@@ -0,0 +1,13 @@
+package com.tiendavinos.service;
+
+import java.util.Optional;
+
+import com.tiendavinos.model.Producto;
+
+public interface ProductoService {
+	public Producto save(Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+
+}
---
+++ src/main/java/com/tiendavinos/service/ProductoServiceImplementacion.java
@@ -0,0 +1,41 @@
+package com.tiendavinos.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiendavinos.model.Producto;
+import com.tiendavinos.repository.ProductoRepository;
+
+@Service
+public class ProductoServiceImplementacion implements ProductoService{
+	
+	@Autowired
+	private ProductoRepository productoRepository;
+
+	@Override
+	public Producto save(Producto producto) {
+		// TODO Auto-generated method stub
+		return productoRepository.save(producto);
+	}
+
+	@Override
+	public Optional<Producto> get(Integer id) {
+		// TODO Auto-generated method stub
+		return productoRepository.findById(id);
+	}
+
+	@Override
+	public void update(Producto producto) {
+		productoRepository.save(producto);
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		productoRepository.deleteById(id);
+		
+	}
+
+}
