e5a3d0889b4e1e8b40eb792e139205ab9918d7b8
SapiTello <saidapi@hotmail.com>
2024-07-30 23:12:02 -0500

    funcionalidad de ver producto

--- src/main/java/com/tiendavinos/controller/HomeController.java
+++ src/main/java/com/tiendavinos/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.tiendavinos.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tiendavinos.service.ProductoService;
@@ -11,6 +14,8 @@ import com.tiendavinos.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private  ProductoService productoService;
@@ -21,6 +26,12 @@ public class HomeController {
 		model.addAttribute("productos",productoService.findAll());
 		
 		return "usuario/home";
+	}
+	
+	@GetMapping("productohome/{idProducto}")
+	public String productoHome(@PathVariable Integer idProducto) {
+		log.info("Id producto enviado como parametro {}",idProducto);
+		return "usuario/productohome";
 	}
 
 }
